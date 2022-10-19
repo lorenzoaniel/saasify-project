21816e8634382dfd2b8ccaa4aa2df5c4ae6b4e47
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-18 19:07:16 -0600

    created style files for brands components
    brands div and img css style files for the defaultimgcontainer component

---
+++ src/Styles/main-landing-brands-style/main-landing-brands-img-style/main-landing-brands-img-style.js
@@ -0,0 +1,24 @@
+import { css } from "styled-components";
+
+export const mainLandingBrandsImgStyle = css`
+	.slack-icon {
+		width: 168.81px;
+		height: 42px;
+	}
+	.airbnb-icon {
+		width: 167.82px;
+		height: 76px;
+	}
+	.amazon-icon {
+		width: 144.13px;
+		height: 44px;
+	}
+	.google-icon {
+		width: 132.28px;
+		height: 44px;
+	}
+	.netflix-icon {
+		width: 146.1px;
+		height: 44px;
+	}
+`;
