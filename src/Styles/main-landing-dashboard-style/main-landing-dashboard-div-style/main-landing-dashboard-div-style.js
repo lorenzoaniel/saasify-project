8deb6640abd53db12d6083060b744b2631d9e0ac
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-18 19:07:35 -0600

    Update main-landing-dashboard-div-style.js
    converted to percentage

--- src/Styles/main-landing-dashboard-style/main-landing-dashboard-div-style/main-landing-dashboard-div-style.js
+++ src/Styles/main-landing-dashboard-style/main-landing-dashboard-div-style/main-landing-dashboard-div-style.js
@@ -4,7 +4,7 @@ export const mainLandingDashboardDivStyle = css`
 	width: 80%;
 	border-radius: 21.7647px;
 	position: absolute;
-	top: 700px;
+	top: 75%;
 	left: 9.5%;
 	box-shadow: 0 0 0.5rem 0.5rem rgba(0, 0, 0, 0.5);
 `;
