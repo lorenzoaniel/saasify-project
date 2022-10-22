56f14fc370c40b3c6f51d945debad3e460d2e4f1
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 15:56:29 -0600

    Update App.js
    converted to styled component incase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import styled from "styled-components";
 import MainLandingSection from "./Sections/MainLandingSection/MainLandingSection";
 import { GlobalBaseStyle } from "./Styles/GlobalBaseStyle/GlobalBaseStyle";
+
+const AppContainer = styled.div``;
 
 function App() {
 	return (
 		<>
 			<GlobalBaseStyle />
-			<div className="App">
+			<AppContainer className={"App-Container"}>
 				<MainLandingSection />
-			</div>
+			</AppContainer>
 		</>
 	);
 }
