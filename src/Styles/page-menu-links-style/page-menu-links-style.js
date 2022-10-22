6380872252a0cffacdb484c52715535fec00fdc2
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 16:02:38 -0600

    Update page-menu-links-style.js

--- src/Styles/page-menu-links-style/page-menu-links-style.js
+++ src/Styles/page-menu-links-style/page-menu-links-style.js
@@ -2,7 +2,7 @@ import { css } from "styled-components";
 
 export const pageMenuLinksStyle = css`
 	height: 100%;
-	width: 40%;
+	width: 50%;
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
