0b4d5372771024f9fedc3abd063e9ff87b51f6fb
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 16:00:18 -0600

    Update NavBar.js
    padding and width update

--- src/Components/MainLandingSection/NavBar/NavBar.js
+++ src/Components/MainLandingSection/NavBar/NavBar.js
@@ -2,17 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const NavBarContainer = styled.div`
-	height: 42px;
-	width: 85%;
+	height: 90px; //42px base plus margin
+	width: 100%;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 
-	grid-area: ${(props) => props.className};
-	justify-self: center;
-	align-self: flex-start;
-
-	margin-top: 39px;
+	padding-top: 39px;
 `;
 
 const NavBar = (props) => {
