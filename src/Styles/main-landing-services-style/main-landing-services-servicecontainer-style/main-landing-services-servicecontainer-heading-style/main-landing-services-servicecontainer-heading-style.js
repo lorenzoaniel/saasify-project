cc470be52ddd376ec05b24a74d26997508af2aee
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 17:06:49 -0600

    Create main-landing-services-servicecontainer-heading-style.js
    font styles straight from design file

---
+++ src/Styles/main-landing-services-style/main-landing-services-servicecontainer-style/main-landing-services-servicecontainer-heading-style/main-landing-services-servicecontainer-heading-style.js
@@ -0,0 +1,23 @@
+import { css } from "styled-components";
+
+export const mainLandingServicesServicecontainerHeadingStyle = css`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	font-family: var(--DefaultFont-inter);
+	font-weight: 700;
+	font-size: 24px;
+	line-height: 100%;
+	text-align: center;
+
+	grid-area: mainheading;
+	justify-self: center;
+
+	/* identical to box height, or 24px */
+	letter-spacing: -0.03em;
+
+	color: #000000;
+`;
