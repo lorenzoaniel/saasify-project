63ac3b3f57ee402e99d35d39d0bdc6b9bf1766ed
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 16:01:15 -0600

    Update MainLandingSection.js

--- src/Sections/MainLandingSection/MainLandingSection.js
+++ src/Sections/MainLandingSection/MainLandingSection.js
@@ -9,6 +9,8 @@ import DefaultButton from "../../Components/Common/DefaultButton/DefaultButton";
 import DefaultHeading from "../../Components/Common/DefaultHeading/DefaultHeading";
 import EmailBar from "../../Components/MainLandingSection/EmailBar/EmailBar";
 import DefaultImgContainer from "../../Components/Common/DefaultImgContainer/DefaultImgContainer";
+import Brands from "../../Components/MainLandingSection/Brands/Brands";
+import Services from "../../Components/MainLandingSection/Services/Services";
 
 import { pageMenuLinksStyle } from "../../Styles/page-menu-links-style/page-menu-links-style";
 import { pageMenuLinksButtonStyle } from "../../Styles/page-menu-links-style/page-menu-links-button-style/page-menu-links-button-style";
@@ -21,71 +23,64 @@ import { mainLandingSecondHeadingStyle } from "../../Styles/main-landing-heading
 import { mainLandingDashboardDivStyle } from "../../Styles/main-landing-dashboard-style/main-landing-dashboard-div-style/main-landing-dashboard-div-style.js";
 import { mainLandingDashboardImgStyle } from "../../Styles/main-landing-dashboard-style/main-landing-dashboard-img-style/main-landing-dashboard-img-style.js";
 
-import dashboardimg from "../../Assets/Imgs/dashboard_saasify_example.png";
-import Brands from "../../Components/MainLandingSection/Brands/Brands";
+import dashboardimg from "../../Assets/Imgs/Dashboard/dashboard_saasify_example.png";
+import wavebackground from "../../Assets/Imgs/MainLandingSection/wavebackground.png";
 
 const MainLandingSectionContainer = styled.div`
-	height: 2502px;
+	/* height: 2502px; */
 	width: 100%;
-	/* background-color: pink; */
-`;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	background: url(${(props) => props.backgroundimageprop});
+	padding: 0 10%;
 
-const MainLandingSectionWaveContainer = styled.div`
-	background: var(--MainLandingSection-bg-gradient);
-	height: 1520.5px;
-	width: 100%;
-	clip-path: var(--clipshape-MainLandingSection-wave);
-
-	display: grid;
-	grid-template-columns: 100%;
-	grid-template-rows: 10% 20% 5% 10% 55%;
-	grid-template-areas: "navbar" "mainlandingheading" "mainlandingsecondheading" "registeremail" ".";
+	background-repeat: no-repeat;
+	background-size: contain;
 `;
 
 const MainLandingSection = () => {
 	return (
-		<MainLandingSectionContainer>
-			<MainLandingSectionWaveContainer>
-				<NavBar classnameprop={"navbar"}>
-					<NavBarMenu classnameprop={"page-menu-logo"} customstyleprop={pageMenuLogoContainerStyle}>
-						<Logo
-							titleprop={"Saasify"}
-							classnameprop={"logo-saasify"}
-							customstyleprop={pageMenuLogoStyle}
-						/>
-					</NavBarMenu>
-					<NavBarMenu classnameprop={"page-menu-links"} customstyleprop={pageMenuLinksStyle}>
-						<DefaultLink linktitleprop={"Product"} customstyleprop={pageMenuLinksButtonStyle} />
-						<DefaultLink linktitleprop={"Solution"} customstyleprop={pageMenuLinksButtonStyle} />
-						<DefaultLink linktitleprop={"Customer"} customstyleprop={pageMenuLinksButtonStyle} />
-						<DefaultLink linktitleprop={"Pricing"} customstyleprop={pageMenuLinksButtonStyle} />
-						<DefaultLink linktitleprop={"About us"} customstyleprop={pageMenuLinksButtonStyle} />
-					</NavBarMenu>
-					<NavBarMenu
-						classnameprop={"page-menu-signup"}
-						customstyleprop={pageMenuSignupContainerStyle}
-					>
-						<DefaultButton
-							classnameprop={"navbar-signup"}
-							btntitleprop={"Sign up"}
-							customstyleprop={pageMenuSignupButtonStyle}
-						/>
-					</NavBarMenu>
-				</NavBar>
-				<DefaultHeading
-					headingtitleprop={"The Fastest Way For Startups To Do Any Analysis"}
-					customstyleprop={mainLandingHeadingStyle}
-					classnameprop={"mainlandingheading"}
-				/>
-				<DefaultHeading
-					headingtitleprop={
-						"Equals is the only spreadsheet with built-in connections to any database, versioning, and collaboration"
-					}
-					customstyleprop={mainLandingSecondHeadingStyle}
-					classnameprop={"mainlandingsecondheading"}
-				/>
-				<EmailBar classnameprop={"registeremail"} />
-			</MainLandingSectionWaveContainer>
+		<MainLandingSectionContainer backgroundimageprop={wavebackground}>
+			<NavBar classnameprop={"navbar"}>
+				<NavBarMenu classnameprop={"page-menu-logo"} customstyleprop={pageMenuLogoContainerStyle}>
+					<Logo
+						titleprop={"Saasify"}
+						classnameprop={"logo-saasify"}
+						customstyleprop={pageMenuLogoStyle}
+					/>
+				</NavBarMenu>
+				<NavBarMenu classnameprop={"page-menu-links"} customstyleprop={pageMenuLinksStyle}>
+					<DefaultLink linktitleprop={"Product"} customstyleprop={pageMenuLinksButtonStyle} />
+					<DefaultLink linktitleprop={"Solution"} customstyleprop={pageMenuLinksButtonStyle} />
+					<DefaultLink linktitleprop={"Customer"} customstyleprop={pageMenuLinksButtonStyle} />
+					<DefaultLink linktitleprop={"Pricing"} customstyleprop={pageMenuLinksButtonStyle} />
+					<DefaultLink linktitleprop={"About us"} customstyleprop={pageMenuLinksButtonStyle} />
+				</NavBarMenu>
+				<NavBarMenu
+					classnameprop={"page-menu-signup"}
+					customstyleprop={pageMenuSignupContainerStyle}
+				>
+					<DefaultButton
+						classnameprop={"navbar-signup"}
+						btntitleprop={"Sign up"}
+						customstyleprop={pageMenuSignupButtonStyle}
+					/>
+				</NavBarMenu>
+			</NavBar>
+			<DefaultHeading
+				headingtitleprop={"The Fastest Way For Startups To Do Any Analysis"}
+				customstyleprop={mainLandingHeadingStyle}
+				classnameprop={"mainlandingheading"}
+			/>
+			<DefaultHeading
+				headingtitleprop={
+					"Equals is the only spreadsheet with built-in connections to any database, versioning, and collaboration"
+				}
+				customstyleprop={mainLandingSecondHeadingStyle}
+				classnameprop={"mainlandingsecondheading"}
+			/>
+			<EmailBar classnameprop={"registeremail"} />
 			<DefaultImgContainer
 				customstyledivprop={mainLandingDashboardDivStyle}
 				customstyleimgprop={mainLandingDashboardImgStyle}
@@ -93,6 +88,7 @@ const MainLandingSection = () => {
 				srcprop={dashboardimg}
 			/>
 			<Brands />
+			<Services />
 		</MainLandingSectionContainer>
 	);
 };
