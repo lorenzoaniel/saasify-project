5468c9a2b784b77f73e1775cfbf7ad4dfa43046f
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 17:05:28 -0600

    Update Services.js
    added custom style props and tweaked grid for each service container( this houses icon, main heading, second heading etc components).

--- src/Components/MainLandingSection/Services/Services.js
+++ src/Components/MainLandingSection/Services/Services.js
@@ -6,18 +6,20 @@ import DefaultHeading from "../../Common/DefaultHeading/DefaultHeading";
 import DefaultImgContainer from "../../Common/DefaultImgContainer/DefaultImgContainer";
 
 import { mainLandingServicesMainTitleStyle } from "../../../Styles/main-landing-services-style/main-landing-services-main-title-style/main-landing-services-main-title-style.js";
+import { mainLandingServicesServicecontainerIconStyle } from "../../../Styles/main-landing-services-style/main-landing-services-servicecontainer-style/main-landing-services-servicecontainer-icon-style/main-landing-services-servicecontainer-icon-style.js";
+import { mainLandingServicesServicecontainerHeadingStyle } from "../../../Styles/main-landing-services-style/main-landing-services-servicecontainer-style/main-landing-services-servicecontainer-heading-style/main-landing-services-servicecontainer-heading-style.js";
+import { mainLandingServicesServicecontainerSecondheadingStyle } from "../../../Styles/main-landing-services-style/main-landing-services-servicecontainer-style/main-landing-services-servicecontainer-secondheading-style/main-landing-services-servicecontainer-secondheading-style.js";
 
 import taskmanagementimg from "../../../Assets/Imgs/Services/taskmanagementicon.png";
 import designsystemimg from "../../../Assets/Imgs/Services/designsystemicon.png";
 import bestpracticeimg from "../../../Assets/Imgs/Services/bestpracticeicon.png";
 
 const ServicesMainContainer = styled.div`
-	background-color: pink;
 	width: 100%;
 	height: 550px;
 	display: grid;
 	grid-template-columns: 100%;
-	grid-template-rows: 1fr 70%;
+	grid-template-rows: 25% 70%;
 	grid-template-areas: "service-main-title" "service-container-container";
 	row-gap: 5%;
 
@@ -32,25 +34,22 @@ const ServicesContainerContainer = styled.div`
 `;
 
 const ServicesContainer = styled.div`
-	background-color: orange;
+	background: #ffffff;
 	height: 90%;
 	width: 30%;
 
-	display: flex;
-	flex-direction: column;
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: 25% 20% 35% 5%;
+	grid-template-areas: "icon" "mainheading" "secondheading" "readmore";
 	justify-content: space-around;
 	align-items: center;
+	grid-row-gap: 5%;
 
-	/* font-family: var(--DefaultFont-inter);
-	font-weight: 700;
-	font-size: 24px;
-	line-height: 100%; */
-
-	/* identical to box height, or 24px */
-	letter-spacing: -0.03em;
-
-	padding: 1rem;
+	padding: 2%;
 	border-radius: 1rem;
+	box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.07);
+	border-radius: 20px;
 `;
 
 const Services = (props) => {
@@ -66,42 +65,57 @@ const Services = (props) => {
 			<ServicesContainerContainer>
 				<ServicesContainer className={"service-task-management"}>
 					<DefaultImgContainer
+						customstyledivprop={mainLandingServicesServicecontainerIconStyle}
 						srcprop={taskmanagementimg}
 						classnameprop={"service-task-management-icon"}
 					/>
-					<DefaultHeading headingtitleprop={"Task Management"} />
 					<DefaultHeading
+						customstyleprop={mainLandingServicesServicecontainerHeadingStyle}
+						headingtitleprop={"Task Management"}
+					/>
+					<DefaultHeading
+						customstyleprop={mainLandingServicesServicecontainerSecondheadingStyle}
 						headingtitleprop={
 							"The process of managing a task through its life cycle. It involves planning, testing, tracking."
 						}
 					/>
-					<DefaultButton />
+					<DefaultButton btntitleprop={"Read More"} />
 				</ServicesContainer>
 				<ServicesContainer className={"service-design-system"}>
 					<DefaultImgContainer
+						customstyledivprop={mainLandingServicesServicecontainerIconStyle}
 						srcprop={designsystemimg}
 						classnameprop={"service-design-system-icon"}
 					/>
-					<DefaultHeading headingtitleprop={"Design System"} />
 					<DefaultHeading
+						customstyleprop={mainLandingServicesServicecontainerHeadingStyle}
+						headingtitleprop={"Design System"}
+					/>
+					<DefaultHeading
+						customstyleprop={mainLandingServicesServicecontainerSecondheadingStyle}
 						headingtitleprop={
 							"The technology by which a process or procedure is preformed with minimal human assistance."
 						}
 					/>
-					<DefaultButton />
+					<DefaultButton btntitleprop={"Read More"} />
 				</ServicesContainer>
 				<ServicesContainer className={"service-best-practice"}>
 					<DefaultImgContainer
+						customstyledivprop={mainLandingServicesServicecontainerIconStyle}
 						srcprop={bestpracticeimg}
 						classnameprop={"service-best-practice-icon"}
 					/>
-					<DefaultHeading headingtitleprop={"Best Practices"} />
 					<DefaultHeading
+						customstyleprop={mainLandingServicesServicecontainerHeadingStyle}
+						headingtitleprop={"Best Practices"}
+					/>
+					<DefaultHeading
+						customstyleprop={mainLandingServicesServicecontainerSecondheadingStyle}
 						headingtitleprop={
 							"Creating plan to spend your money, This spending planer is called a budgeting tool for business."
 						}
 					/>
-					<DefaultButton />
+					<DefaultButton btntitleprop={"Read More"} />
 				</ServicesContainer>
 			</ServicesContainerContainer>
 		</ServicesMainContainer>
