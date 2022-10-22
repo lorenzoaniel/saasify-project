79d0798799ed1d401ab98b6cac92373d4c00fa3d
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 17:07:21 -0600

    Create main-landing-services-servicecontainer-secondheading-style.js

---
+++ src/Styles/main-landing-services-style/main-landing-services-servicecontainer-style/main-landing-services-servicecontainer-secondheading-style/main-landing-services-servicecontainer-secondheading-style.js
@@ -0,0 +1,17 @@
+import { css } from "styled-components";
+
+export const mainLandingServicesServicecontainerSecondheadingStyle = css`
+	font-family: var(--DefaultFont-inter);
+	font-weight: 500;
+	font-size: 16px;
+	line-height: 25px;
+
+	/* or 188% */
+	text-align: center;
+	letter-spacing: -0.02em;
+	word-wrap: break-word;
+
+	color: #000000;
+
+	opacity: 0.7;
+`;
