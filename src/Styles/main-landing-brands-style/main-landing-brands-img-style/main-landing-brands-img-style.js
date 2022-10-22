802cbe544813553b1d4bb92316884ce9db80eca8
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 16:01:52 -0600

    Update main-landing-brands-img-style.js

--- src/Styles/main-landing-brands-style/main-landing-brands-img-style/main-landing-brands-img-style.js
+++ src/Styles/main-landing-brands-style/main-landing-brands-img-style/main-landing-brands-img-style.js
@@ -1,23 +1,23 @@
 import { css } from "styled-components";
 
 export const mainLandingBrandsImgStyle = css`
-	.slack-icon {
+	&#slack-icon {
 		width: 168.81px;
 		height: 42px;
 	}
-	.airbnb-icon {
+	&#airbnb-icon {
 		width: 167.82px;
 		height: 76px;
 	}
-	.amazon-icon {
+	&#amazon-icon {
 		width: 144.13px;
 		height: 44px;
 	}
-	.google-icon {
+	&#google-icon {
 		width: 132.28px;
 		height: 44px;
 	}
-	.netflix-icon {
+	&#netflix-icon {
 		width: 146.1px;
 		height: 44px;
 	}
