89b1c713699d2719ab5200a2ea9082316acc14b9
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 16:02:47 -0600

    Update page-menu-logo-container-style.js

--- src/Styles/page-menu-links-style/page-menu-logo-container-style/page-menu-logo-container-style.js
+++ src/Styles/page-menu-links-style/page-menu-logo-container-style/page-menu-logo-container-style.js
@@ -1,7 +1,7 @@
 import { css } from "styled-components";
 
 export const pageMenuLogoContainerStyle = css`
-	width: 30%;
+	width: 25%;
 	height: 100%;
 	display: flex;
 	justify-content: flex-start;
