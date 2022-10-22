a960a61260d164c128f291c55cb7e21c5969d3f9
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 16:02:34 -0600

    Create main-landing-services-main-title-style.js

---
+++ src/Styles/main-landing-services-style/main-landing-services-main-title-style/main-landing-services-main-title-style.js
@@ -0,0 +1,20 @@
+import { css } from "styled-components";
+
+export const mainLandingServicesMainTitleStyle = css`
+	width: 470px;
+	height: 126px;
+	grid-area: service-main-title;
+	justify-self: center;
+
+	font-family: var(--DefaultFont-inter);
+	font-weight: 700;
+	font-size: 50px;
+	line-height: 125%;
+
+	/* or 62px */
+	text-align: center;
+	letter-spacing: 0.005rem;
+	text-transform: capitalize;
+
+	color: #000000;
+`;
