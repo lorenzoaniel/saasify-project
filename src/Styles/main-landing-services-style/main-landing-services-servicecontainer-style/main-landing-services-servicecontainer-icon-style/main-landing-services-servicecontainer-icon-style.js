abc2eec723a0d0e738e496163f393224f9ad17fd
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-22 17:07:28 -0600

    Create main-landing-services-servicecontainer-icon-style.js

---
+++ src/Styles/main-landing-services-style/main-landing-services-servicecontainer-style/main-landing-services-servicecontainer-icon-style/main-landing-services-servicecontainer-icon-style.js
@@ -0,0 +1,9 @@
+import { css } from "styled-components";
+
+export const mainLandingServicesServicecontainerIconStyle = css`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
