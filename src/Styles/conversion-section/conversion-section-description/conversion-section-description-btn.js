739ef09d3f01006cceba3710ed63617dbf99b370
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-23 20:37:30 -0600

    Create conversion-section-description-btn.js
    description css for conversion section

---
+++ src/Styles/conversion-section/conversion-section-description/conversion-section-description-btn.js
@@ -0,0 +1,26 @@
+import { css } from "styled-components";
+
+export const conversionSectionDescriptionBtn = css`
+	width: 215px;
+	height: 68px;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	padding: 15px 40px;
+	gap: 10px;
+
+	background: linear-gradient(106.75deg, #2272ff 0%, #0d03c3 100%);
+	border-radius: 40px;
+	border: none;
+
+	font-family: var(--DefaultFont-inter);
+	font-weight: 700;
+	font-size: 20px;
+	line-height: 30px;
+	letter-spacing: -0.02em;
+
+	color: #ffffff;
+	margin-top: 35px;
+`;
