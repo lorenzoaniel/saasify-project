fdc3c66a1fea765619423f651eec659e0d8340e4
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-28 19:55:23 -0600

    Update App.js
    added new components, videosection being the component with the video and steps component and the plans and testimonials is currently still being constructed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
+
 import ConversionSection from "./Sections/ConversionSection/ConversionSection";
 import MainLandingSection from "./Sections/MainLandingSection/MainLandingSection";
+import PlansTestimonialSection from "./Sections/PlansTestimonialSection/PlansTestimonialSection";
+import VideoSection from "./Sections/VideoSection/VideoSection";
+
 import { GlobalBaseStyle } from "./Styles/GlobalBaseStyle/GlobalBaseStyle";
 
 const AppContainer = styled.div``;
@@ -12,6 +16,8 @@ function App() {
 			<AppContainer className={"App-Container"}>
 				<MainLandingSection />
 				<ConversionSection />
+				<VideoSection />
+				<PlansTestimonialSection />
 			</AppContainer>
 		</>
 	);
