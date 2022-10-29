e68d57d5db93c0185ed2ab54ca7075ba2f698d93
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-28 19:59:00 -0600

    Create video-section-getstarted-getstartedbutton-style.js
    get started button on video section

---
+++ src/Styles/video-section/video-section-getstarted-style/video-section-getstarted-getstartedbutton-style/video-section-getstarted-getstartedbutton-style.js
@@ -0,0 +1,17 @@
+import { css } from "styled-components";
+
+export const videoSectionGetstartedGetstartedbuttonStyle = css`
+	width: 225px;
+	height: 67px;
+	background-color: #ffffff;
+	border-radius: 40px;
+	border: none;
+
+	font-family: var(--DefaultFont-inter);
+	font-weight: 700;
+	font-size: 20px;
+	line-height: 30px;
+	text-align: center;
+	letter-spacing: -0.02em;
+	color: #0f0bc7;
+`;
