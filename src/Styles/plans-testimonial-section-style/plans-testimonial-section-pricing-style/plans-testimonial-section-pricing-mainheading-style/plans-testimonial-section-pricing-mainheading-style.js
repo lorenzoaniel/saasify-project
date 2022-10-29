843594723e957bee4ba02640a7a7d04683a3e3f9
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-28 19:58:45 -0600

    Create plans-testimonial-section-pricing-mainheading-style.js
    "Get results...etc" heading

---
+++ src/Styles/plans-testimonial-section-style/plans-testimonial-section-pricing-style/plans-testimonial-section-pricing-mainheading-style/plans-testimonial-section-pricing-mainheading-style.js
@@ -0,0 +1,18 @@
+import { css } from "styled-components";
+
+export const plansTestimonialSectionPricingMainheadingStyle = css`
+	/* Get results first. Pick a plan later. */
+
+	width: 812px;
+
+	font-family: var(--DefaultFont-inter);
+	font-weight: 700;
+	font-size: 50px;
+	line-height: 61px;
+
+	/* identical to box height */
+	text-align: center;
+	letter-spacing: 0.005em;
+
+	color: #000000;
+`;
