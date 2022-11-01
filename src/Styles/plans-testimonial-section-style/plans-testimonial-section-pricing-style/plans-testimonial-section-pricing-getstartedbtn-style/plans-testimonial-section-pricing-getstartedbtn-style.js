5711b7e4f5223acfad4d8fb0a6ab296c39b57c3e
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-31 20:14:41 -0600

    Create plans-testimonial-section-pricing-getstartedbtn-style.js

---
+++ src/Styles/plans-testimonial-section-style/plans-testimonial-section-pricing-style/plans-testimonial-section-pricing-getstartedbtn-style/plans-testimonial-section-pricing-getstartedbtn-style.js
@@ -0,0 +1,26 @@
+import { css } from "styled-components";
+
+export const plansTestimonialSectionPricingGetStartedBtnStyle = css`
+	width: 206px;
+	height: 58px;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	padding: 20px 5px;
+	gap: 10px;
+
+	background: linear-gradient(106.75deg, #2272ff 0%, #0d03c3 100%);
+	border-radius: 66px;
+	border: none;
+
+	font-family: "Inter";
+	font-style: normal;
+	font-weight: 700;
+	font-size: 18px;
+	line-height: 100%;
+	text-align: center;
+	letter-spacing: 0.011em;
+	color: #ffffff;
+`;
