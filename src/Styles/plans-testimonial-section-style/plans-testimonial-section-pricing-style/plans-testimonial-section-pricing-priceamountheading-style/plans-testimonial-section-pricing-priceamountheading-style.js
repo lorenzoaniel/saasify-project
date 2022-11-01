57c500941ed24b3dff1f76250231da73fb01540e
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-31 20:15:06 -0600

    Create plans-testimonial-section-pricing-priceamountheading-style.js

---
+++ src/Styles/plans-testimonial-section-style/plans-testimonial-section-pricing-style/plans-testimonial-section-pricing-priceamountheading-style/plans-testimonial-section-pricing-priceamountheading-style.js
@@ -0,0 +1,12 @@
+import { css } from "styled-components";
+
+export const plansTestimonialSectionPricingPriceAmountHeadingStyle = css`
+	width: 113px;
+	height: 60px;
+
+	font-family: var(--DefaultFont-inter);
+	font-weight: 700;
+	font-size: 50px;
+	line-height: 60px;
+	color: #0f0bc7;
+`;
