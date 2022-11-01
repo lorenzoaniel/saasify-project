fb0dff8e7485ff913077c34b994f86a45b36ad45
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-31 20:15:11 -0600

    Create plans-testimonial-section-pricing-pricetimerange-style.js

---
+++ src/Styles/plans-testimonial-section-style/plans-testimonial-section-pricing-style/plans-testimonial-section-pricing-pricetimerange-style/plans-testimonial-section-pricing-pricetimerange-style.js
@@ -0,0 +1,14 @@
+import { css } from "styled-components";
+
+export const plansTestimonialSectionPricingPriceTimeRangeStyle = css`
+	width: 68px;
+	height: 24px;
+
+	font-family: var(--DefaultFont-inter);
+	font-weight: 500;
+	font-size: 16px;
+	line-height: 24px;
+	letter-spacing: 0.011em;
+	color: #000000;
+	opacity: 0.6;
+`;
