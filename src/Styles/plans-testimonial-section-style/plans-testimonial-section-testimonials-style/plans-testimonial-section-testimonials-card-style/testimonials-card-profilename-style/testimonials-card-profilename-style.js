91773d0a8fad76e1d1c761995b45415da7acabd7
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-08 14:22:02 -0700

    Create testimonials-card-profilename-style.js

---
+++ src/Styles/plans-testimonial-section-style/plans-testimonial-section-testimonials-style/plans-testimonial-section-testimonials-card-style/testimonials-card-profilename-style/testimonials-card-profilename-style.js
@@ -0,0 +1,13 @@
+import { css } from "styled-components";
+
+export const plansTestimonialSectionTestimonialsCardProfilenameStyle = css`
+	width: 100%;
+	height: 15%;
+
+	font-family: var(--DefaultFont-inter);
+	font-weight: 700;
+	font-size: 16px;
+	line-height: 19px;
+
+	color: #000000;
+`;
