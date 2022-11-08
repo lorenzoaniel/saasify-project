635d4a5de47ae8d97e3145342705f4e24bfe38a9
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-08 13:49:12 -0700

    Create footer-section-links-container-linkslogo-logo-style.js

---
+++ src/Styles/footer-section/footer-section-links-container-style/footer-section-links-container-linkslogo-style/footer-section-links-container-linkslogo-logo-style/footer-section-links-container-linkslogo-logo-style.js
@@ -0,0 +1,11 @@
+import { css } from "styled-components";
+
+export const footerSectionLinksContainerLinkslogoLogoStyle = css`
+	font-family: var(--DefaultFont-inter);
+	font-weight: 700;
+	font-size: 20px;
+	line-height: 24px;
+	text-transform: uppercase;
+
+	color: #ffffff;
+`;
