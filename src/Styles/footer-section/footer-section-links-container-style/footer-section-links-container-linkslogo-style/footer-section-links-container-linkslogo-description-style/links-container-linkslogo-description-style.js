0e324c93da1cf57cb9be75a669a72e671841712c
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-08 14:01:07 -0700

    Update links-container-linkslogo-description-style.js

--- src/Styles/footer-section/footer-section-links-container-style/footer-section-links-container-linkslogo-style/footer-section-links-container-linkslogo-description-style/links-container-linkslogo-description-style.js
+++ src/Styles/footer-section/footer-section-links-container-style/footer-section-links-container-linkslogo-style/footer-section-links-container-linkslogo-description-style/links-container-linkslogo-description-style.js
@@ -1,1 +1,15 @@
+import { css } from "styled-components";
 
+export const footerSectionLinksContainerLinkslogoDescriptionStyle = css`
+	font-family: var(--DefaultFont-inter);
+	font-weight: 400;
+	font-size: 18px;
+	line-height: 150%;
+
+	/* or 27px */
+	letter-spacing: -0.02em;
+
+	color: #ffffff;
+
+	opacity: 0.8;
+`;
