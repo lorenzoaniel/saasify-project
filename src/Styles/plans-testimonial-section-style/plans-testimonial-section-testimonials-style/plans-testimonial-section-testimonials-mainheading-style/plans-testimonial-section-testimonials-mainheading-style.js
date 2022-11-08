db5687c9309aeefd1acad99f79b44f071cc7e86e
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-08 13:49:38 -0700

    Create plans-testimonial-section-testimonials-mainheading-style.js

---
+++ src/Styles/plans-testimonial-section-style/plans-testimonial-section-testimonials-style/plans-testimonial-section-testimonials-mainheading-style/plans-testimonial-section-testimonials-mainheading-style.js
@@ -0,0 +1,16 @@
+import { css } from "styled-components";
+
+export const plansTestimonialSectionTestimonialsMainheadingStyle = css`
+	display: inline-block;
+	width: 100%;
+	height: 80px;
+
+	font-family: var(--DefaultFont-inter);
+	font-weight: 700;
+	font-size: 50px;
+	line-height: 56px;
+	text-align: center;
+	letter-spacing: -0.03em;
+
+	color: #000000;
+`;
