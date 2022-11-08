0341f8117a0943c8e87189c4b6540db1ddf70412
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-08 13:49:22 -0700

    Create footer-section-mainheading-container-button-style.js

---
+++ src/Styles/footer-section/footer-section-mainheading-container-style/footer-section-mainheading-container-button-style/footer-section-mainheading-container-button-style.js
@@ -0,0 +1,18 @@
+import { css } from "styled-components";
+
+export const footerSectionMainheadingContainerButtonStyle = css`
+	width: 225px;
+	height: 67px;
+
+	background: #ffffff;
+	border-radius: 500px;
+	border: none;
+
+	font-family: var(--DefaultFont-inter);
+	font-weight: 700;
+	font-size: 20px;
+	line-height: 30px;
+	text-align: center;
+	letter-spacing: -0.02em;
+	color: #0f0bc7;
+`;
