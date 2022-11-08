b12735de764419a50159fecb27793d05682faccb
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-08 13:49:19 -0700

    Create footer-section-links-container-linksmini-links-style.js

---
+++ src/Styles/footer-section/footer-section-links-container-style/footer-section-links-container-linksmini-style/footer-section-links-container-linksmini-links-style/footer-section-links-container-linksmini-links-style.js
@@ -0,0 +1,16 @@
+import { css } from "styled-components";
+
+export const footerSectionLinksContainerLinksminiLinksStyle = css`
+	font-family: var(--DefaultFont-inter);
+	font-weight: 400;
+	font-size: 16px;
+	line-height: 150%;
+
+	/* identical to box height, or 24px */
+	text-align: justify;
+	letter-spacing: -0.02em;
+
+	color: #ffffff;
+
+	opacity: 0.8;
+`;
