8494696059e7ab38c0670bf2e35c1414f30be5fe
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-08 13:49:26 -0700

    Create footer-section-mainheading-container-heading-style.js

---
+++ src/Styles/footer-section/footer-section-mainheading-container-style/footer-section-mainheading-container-heading-style/footer-section-mainheading-container-heading-style.js
@@ -0,0 +1,10 @@
+import { css } from "styled-components";
+
+export const footerSectionMainheadingContainerHeadingStyle = css`
+	font-family: var(--DefaultFont-inter);
+	font-weight: 800;
+	font-size: 60px;
+	line-height: 67px;
+	text-align: center;
+	color: #ffffff;
+`;
