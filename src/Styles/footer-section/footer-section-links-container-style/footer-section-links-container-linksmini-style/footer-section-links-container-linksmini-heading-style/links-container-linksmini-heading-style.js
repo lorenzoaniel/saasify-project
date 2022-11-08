cd7cafdde768d2a5b08acc4a4fbae3e4892eae50
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-08 14:16:23 -0700

    Create links-container-linksmini-heading-style.js

---
+++ src/Styles/footer-section/footer-section-links-container-style/footer-section-links-container-linksmini-style/footer-section-links-container-linksmini-heading-style/links-container-linksmini-heading-style.js
@@ -0,0 +1,14 @@
+import { css } from "styled-components";
+
+export const footerSectionLinksContainerLinksminiHeadingStyle = css`
+	font-family: var(--DefaultFont-inter);
+	font-weight: 600;
+	font-size: 20px;
+	line-height: 150%;
+
+	/* identical to box height, or 30px */
+	text-align: justify;
+	letter-spacing: -0.02em;
+
+	color: #ffffff;
+`;
