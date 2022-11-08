d6de11f2841445cf5587084224aae8d3b8e9bea1
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-08 14:20:44 -0700

    Create testimonials-card-profilelocation-style.js

---
+++ src/Styles/plans-testimonial-section-style/plans-testimonial-section-testimonials-style/plans-testimonial-section-testimonials-card-style/testimonials-card-profilelocation-style/testimonials-card-profilelocation-style.js
@@ -0,0 +1,10 @@
+import { css } from "styled-components";
+
+export const plansTestimonialSectionTestimonialsCardProfilelocationStyle = css`
+	font-family: var(--DefaultFont-inter);
+	font-weight: 400;
+	font-size: 14px;
+	line-height: 17px;
+	color: #000000;
+	opacity: 0.7;
+`;
